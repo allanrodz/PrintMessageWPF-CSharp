45945c924dd28ffa01f15f9e825832b569d8509d
Reception2 <Reception2@DUNLRECEPTILL>
2022-10-06 06:44:26 +0100

    Add project files.

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string fn = txtFN.Text;
+            if (fn == "")
+                fn = "Anonymous";
+            string sn = txtSn.Text;
+            if (sn == "")
+                sn = "Anonymous";
+            string dob = dtpDob.Text.ToString();
+            if (dob == "")
+                dob = "Unknown";
+            string message = txtMessage.Text;
+            if(message == "")
+                message = "NO MESSAGE";
+
+            MessageBox.Show($"Mr. {sn.ToUpper()}, {fn}\nBorn in {dob}\nSent the following message:\n\t{message}");
+        }
+
+    
+    }
+}
